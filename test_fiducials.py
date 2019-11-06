89491890dd9b5d174271eca763730747088e568c
asergeenko <asergeenko@gmail.com>
2019-11-06 14:58:47 -0800

    Actionlib goals

--- test_fiducials.py
+++ test_fiducials.py
@@ -1,34 +1,27 @@
 #!/usr/bin/env python
-
+from __future__ import print_function
 
 import rospy
+
+import sys
+
+import actionlib
+
 from geometry_msgs.msg import PoseStamped
 from fiducial_msgs.msg import FiducialTransformArray,FiducialTransform
 from actionlib_msgs.msg import GoalStatusArray,GoalStatus
-from move_base_msgs.msg import MoveBaseActionGoal
+from move_base_msgs.msg import MoveBaseAction,MoveBaseGoal
 
 import numpy as np
 
 class Test_Fiducials:
-
-
-
     def __init__(self):
         rospy.init_node('test_fiducials', anonymous=False)
-        self.pub = rospy.Publisher('/move_base/goal', MoveBaseActionGoal, queue_size=1)
-        self.goal_counter = 0
         
-
-    def goal_status_callback(self,msg):
-        for m in msg.status_list:
-            if m.goal_id == self.goal_id and m.status == GoalStatus.SUCCEEDED:
-                print ("Goal Id {} reached".format(m.goal_id.id))
-                self.sub_once = rospy.Subscriber("/fiducial_transforms", FiducialTransformArray, self.fiducial_callback)
-                self.goal_status.unregister()
-
 
     def fiducial_callback(self,msg):
         if msg.transforms:
+
             # Get closest fiducial
             closest_fiducial = sorted(msg.transforms,key=lambda i: np.linalg.norm(np.array([i.transform.translation.x,
                 i.transform.translation.y,i.transform.translation.z])))[0]
@@ -39,22 +32,28 @@ class Test_Fiducials:
             pose_st.header.frame_id = msg.header.frame_id
             pose_st.header.stamp = msg.header.stamp
 
-            goal = MoveBaseActionGoal()
-            goal.goal.target_pose = pose_st
-            goal.goal_id.id = str(self.goal_counter)
-            self.goal_counter+=1
-            goal.goal_id.stamp = msg.header.stamp
+            result = self.fiducials_client(pose_st,msg)
 
-            self.goal_id = goal.goal_id
+    def fiducials_client(self, pose_st,msg):
+        client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+        client.wait_for_server()
+        goal = MoveBaseGoal()
+        goal.target_pose = pose_st
+        
+        client.send_goal(goal)
+
+        print ("Goal published\nPose:\n",pose_st)
+        self.sub_once.unregister()
+
+        client.wait_for_result()
+        if client.get_state() == GoalStatus.SUCCEEDED:
+            print("Goal reached")
+            self.sub_once = rospy.Subscriber("/fiducial_transforms", FiducialTransformArray, self.fiducial_callback)
+        
+        return client.get_result()
 
 
-            self.pub.publish(goal)
-            print ("Goal Id {} published".format(goal.goal_id.id))
-            self.goal_status = rospy.Subscriber("/move_base/status",GoalStatusArray,self.goal_status_callback)
-            self.sub_once.unregister()
-
-
-    def test(self):    
+    def test(self):
 
         self.sub_once = rospy.Subscriber("/fiducial_transforms", FiducialTransformArray, self.fiducial_callback)
         while not rospy.is_shutdown():
@@ -65,5 +64,6 @@ if __name__ == '__main__':
     try:
         t = Test_Fiducials()
         t.test()
+
     except rospy.ROSInterruptException:
-        pass
+    	print("Script interrupted", file=sys.stderr)
