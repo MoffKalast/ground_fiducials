13732b4e8437b77d4d17683b090320d2a62a8e0b
asergeenko <asergeenko@gmail.com>
2019-12-28 12:01:31 -0800

    Fix transform

--- test_fiducials.py
+++ test_fiducials.py
@@ -7,58 +7,153 @@ import sys
 
 import actionlib
 
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped,TransformStamped,Twist
 from fiducial_msgs.msg import FiducialTransformArray,FiducialTransform
 from actionlib_msgs.msg import GoalStatusArray,GoalStatus
 from move_base_msgs.msg import MoveBaseAction,MoveBaseGoal
 
+import tf2_ros
+
 import numpy as np
+
+import traceback
+
+from tf.transformations import quaternion_from_euler
+import math
+
+ROTATION_ANGLE = 30
 
 class Test_Fiducials:
     def __init__(self):
         rospy.init_node('test_fiducials', anonymous=False)
+        self.state = 'SEARCH'
+        self.buffer = tf2_ros.Buffer(rospy.Time(30))
+        self.listener = tf2_ros.TransformListener(self.buffer)
+        self.broadcaster = tf2_ros.TransformBroadcaster()
+        self.visited_fiducials = []
+        self.closest_fiducial = None
+
+        self.client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+        self.client.wait_for_server()
+
+        self.num_rotations = 0
+        self.wait_for_stop = 0.0
+
+        self.sub_once = rospy.Subscriber("/fiducial_transforms", FiducialTransformArray, self.fiducial_callback)
+
         
+    def rotate(self):
 
-    def fiducial_callback(self,msg):
-        if msg.transforms:
+      	pose_st = PoseStamped()
+        q_rot = quaternion_from_euler(0, 0, -math.radians(ROTATION_ANGLE))
+      	pose_st.pose.position.x = 0
+      	pose_st.pose.position.y = 0
+        pose_st.pose.position.z = 0
 
-            # Get closest fiducial
-            closest_fiducial = sorted(msg.transforms,key=lambda i: np.linalg.norm(np.array([i.transform.translation.x,
-                i.transform.translation.y,i.transform.translation.z])))[0]
+        pose_st.pose.orientation.x = q_rot[0]
+        pose_st.pose.orientation.y = q_rot[1]
+        pose_st.pose.orientation.z = q_rot[2]
+        pose_st.pose.orientation.w = q_rot[3]
+        pose_st.header.frame_id = "base_link"
+        pose_st.header.stamp = rospy.Time.now()
 
-            pose_st = PoseStamped()
-            pose_st.pose.position = closest_fiducial.transform.translation
-            pose_st.pose.orientation = closest_fiducial.transform.rotation
-            pose_st.header.frame_id = msg.header.frame_id
-            pose_st.header.stamp = msg.header.stamp
-
-            result = self.fiducials_client(pose_st,msg)
-
-    def fiducials_client(self, pose_st,msg):
-        client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
-        client.wait_for_server()
         goal = MoveBaseGoal()
         goal.target_pose = pose_st
-        
-        client.send_goal(goal)
+        self.rotate_goal = self.client.send_goal(goal)
+        self.client.wait_for_result()
 
-        print ("Goal published\nPose:\n",pose_st)
+            
+    def fiducial_callback(self,msg):
+        if msg.transforms:
+            fiducials = sorted([f for f in msg.transforms if f.fiducial_id not in self.visited_fiducials],key=lambda i: np.linalg.norm(np.array([i.transform.translation.x,
+                i.transform.translation.y,i.transform.translation.z])))
+
+            if fiducials:
+                if self.state == 'SEARCH' or (self.state == 'ROTATION' and (self.client.get_result() and self.client.get_state() == GoalStatus.SUCCEEDED)):
+                    self.closest_fiducial = fiducials[0]
+
+                    t = TransformStamped()
+                    t.child_frame_id = "fid%d"% self.closest_fiducial.fiducial_id
+                    t.header.frame_id = "base_link"
+                    t.header.stamp = rospy.Time.now()
+            
+                    t.transform.translation = self.closest_fiducial.transform.translation
+                    t.transform.rotation = self.closest_fiducial.transform.rotation
+
+                    self.broadcaster.sendTransform(t)
+
+                    try:
+            	        tf = self.buffer.lookup_transform("base_link", t.child_frame_id,rospy.Time.now(),rospy.Duration(1.0))
+            	        pose_st = PoseStamped()
+            	        pose_st.pose.position = tf.transform.translation
+
+                        pose_st.pose.orientation = tf.transform.rotation
+                
+
+            	        pose_st.header.frame_id = msg.header.frame_id
+            	        pose_st.header.stamp = msg.header.stamp
+            	
+            	        result = self.fiducials_client(pose_st,msg)
+
+
+                    except:
+                        traceback.print_exc()
+                        print ("Could not get transform for %s" % self.closest_fiducial.fiducial_id)
+            
+            else:
+                print ("Can't find any fiducials. Finishing the script...")
+                self.state = 'FINISHING'
+            	
+
+    def fiducials_client(self, pose_st,msg):
+        goal = MoveBaseGoal()
+        goal.target_pose = pose_st
+        self.client.send_goal(goal)
+
+      
+        print ("Goal published\n", self.closest_fiducial)
         self.sub_once.unregister()
+        self.state = 'MOVING_TO_FID'
+        self.num_rotations = 0
 
-        client.wait_for_result()
-        if client.get_state() == GoalStatus.SUCCEEDED:
+        success = self.client.wait_for_result()
+
+        if success and self.client.get_state() == GoalStatus.SUCCEEDED:
             print("Goal reached")
+            self.visited_fiducials.append(self.closest_fiducial.fiducial_id)
+
+            self.state = 'SEARCH'
             self.sub_once = rospy.Subscriber("/fiducial_transforms", FiducialTransformArray, self.fiducial_callback)
+            self.closest_fiducial = None
+            
+        else:
+        	self.client.cancel_goal()
         
-        return client.get_result()
+        return self.client.get_result()
 
 
     def test(self):
-
-        self.sub_once = rospy.Subscriber("/fiducial_transforms", FiducialTransformArray, self.fiducial_callback)
+        sleep_time = 0.05
         while not rospy.is_shutdown():
-            rospy.sleep(0.05) 
-
+            rospy.sleep(sleep_time)
+            if self.state == 'SEARCH':
+                if self.wait_for_stop<0.2:
+                    self.wait_for_stop+=sleep_time
+                else:
+                    self.wait_for_stop = 0.0            	
+            	    self.state = 'ROTATION'
+            	    self.rotate()
+            elif self.state == 'ROTATION':
+                if self.client.get_result() and self.client.get_state() == GoalStatus.SUCCEEDED:
+                    self.state = 'SEARCH'
+            	    if self.num_rotations > 360.0/ROTATION_ANGLE:
+                        print("Can't find any fiducials. Finishing the script...")
+                        self.state = 'FINISHING'
+                    else:
+                        self.num_rotations+=1
+                        self.rotate()
+            elif self.state == 'FINISHING':
+                break
 
 if __name__ == '__main__':
     try:
